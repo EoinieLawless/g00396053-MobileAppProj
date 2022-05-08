25a5ee6c4c87d69ebb751a1bc955babc4b2b5196
Eoin <eoinie.lawless@gmail.com>
2022-05-08 23:40:28 +0100

    commit message

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -8,14 +8,21 @@ import { WeatherService } from '../Service/weather.service';
 })
 export class WeatherPage implements OnInit {
 weathers:any = [];
+temps:any = [];
+winds:any = [];
 galwayWeather:any = null;
+
   constructor(private service:WeatherService) { }
 
   ngOnInit() {
     this.service.GetWeatherData().subscribe((data)=>{
       this.weathers = data.weather;
       this.galwayWeather = this.weathers[0];
+      this.temps = data.main;
+      this.winds = data.wind;
       console.log(this.weathers);
+      console.log(this.temps);
+      console.log(this.winds);
   })}
 
 }
